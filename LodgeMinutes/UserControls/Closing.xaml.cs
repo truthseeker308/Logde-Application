fce57ff4b064a75ee2203c2f0ddb2ab789a9bab1
Justin LeCheminant <jlechem@gmail.com>
2017-04-19 16:15:18 -0600

    Getting rid of a bunch of files we don't need in source control.

--- LodgeMinutes/UserControls/Closing.xaml.cs
+++ LodgeMinutes/UserControls/Closing.xaml.cs
@@ -1,4 +1,5 @@
-﻿using LodgeMinutesMiddleWare.Models;
+﻿using LodgeMinutesMiddleWare.Helpers;
+using LodgeMinutesMiddleWare.Models;
 using LodgeMinutesMiddleWare.Views;
 using System;
 using System.Collections.Generic;
@@ -30,13 +31,104 @@ namespace LodgeMinutes.UserControls
         /// <summary>
         /// Handles the Click event of the buttonCloseMeeting Control.
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
         private void buttonCloseMeeting_Click( object sender, RoutedEventArgs e )
         {
-            // the save event triggers other UI things to happen
-            // in the main window
-            MinutesViewModel.Instance.Save();
+            try
+            {
+                Mouse.OverrideCursor = Cursors.Wait;
+
+                // we need to build some notes on closing
+                StringBuilder sb = new StringBuilder();
+
+                sb.AppendFormat("{0}{0}Lodge closed on {1}{0}Closing Form - {2}", Environment.NewLine, FormattingHelper.GetDateAndTime(), this.comboboxClosingForm.Text );
+
+                // append to our minutes
+                MinutesViewModel.Instance.Notes = String.Concat( MinutesViewModel.Instance.Notes, Environment.NewLine, Environment.NewLine, sb.ToString() );
+
+                // finally save our minutes
+                if( MinutesViewModel.Instance.Save() )
+                {
+                    this.buttonOutputMinutes.IsEnabled = true;
+
+                    MessageBox.Show( "Lodge closed." );
+                }
+                else
+                {
+                    MessageBox.Show( "Error closing lodge." );
+                }
+            }
+            catch( Exception ex )
+            {
+                LogHelper.LogError( ex );
+                MessageBox.Show( "Error closing meeting.", "Error", MessageBoxButton.OK, MessageBoxImage.Error );
+            }
+            finally
+            {
+                Mouse.OverrideCursor = null;
+            }
         }
+
+        /// <summary>
+        /// Handles the SelectionChanged event of the comboboxFromDegree control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="SelectionChangedEventArgs"/> instance containing the event data.</param>
+        private void comboboxFromDegree_SelectionChanged( object sender, SelectionChangedEventArgs e )
+        {
+            // we changed the drop down that says we changed the meeting type so show
+            // that by endable the to degree combo
+            this.comboboxToDegree.IsEnabled = true;
+        }
+
+        /// <summary>
+        /// Handles the Click event of the buttonOutputMinutes control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void buttonOutputMinutes_Click( object sender, RoutedEventArgs e )
+        {
+            // output to word document
+            MinutesViewModel.Instance.Export();
+        }
+
+        /// <summary>
+        /// Handles the SelectionChanged event of the comboboxToDegree control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="SelectionChangedEventArgs"/> instance containing the event data.</param>
+        private void comboboxToDegree_SelectionChanged( object sender, SelectionChangedEventArgs e )
+        {
+            try
+            {
+                if( !String.IsNullOrWhiteSpace( this.comboboxToDegree.Text ) )
+                {
+                    Mouse.OverrideCursor = Cursors.Wait;
+
+                    // we need to build some notes on closing
+                    StringBuilder sb = new StringBuilder();
+
+                    sb.AppendFormat( "{0}Degree type changed From - {1}{0}To - {2}", Environment.NewLine, this.comboboxFromDegree.Text , this.comboboxToDegree.Text );
+
+                    // append to our minutes
+                    MinutesViewModel.Instance.Notes = String.Concat( MinutesViewModel.Instance.Notes, Environment.NewLine, Environment.NewLine, sb.ToString() );
+
+                    // finally save our minutes
+                    MinutesViewModel.Instance.Save();
+                }
+
+            }
+            catch( Exception ex )
+            {
+                LogHelper.LogError( ex );
+                MessageBox.Show( "Error chaging degree type.", "Error", MessageBoxButton.OK, MessageBoxImage.Error );
+            }
+            finally
+            {
+                Mouse.OverrideCursor = null;
+            }
+        }
+
     }
 }
